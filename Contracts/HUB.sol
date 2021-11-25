28851db4495d4dc088e8141ad36bffa44ac50c26
Nethny <92464279+Nethny@users.noreply.github.com>
2021-11-25 17:52:05 +0300

    Update HUB.sol
    add comments

--- Contracts/HUB.sol
+++ Contracts/HUB.sol
@@ -17,6 +17,18 @@ import "Ownable.sol";
 import "Counters.sol";
 
 //@author Nethny [TIIK]
+/*
+	This contract allows you to transfer NFT MRK 
+	to the MAVR network, assigning to the specified 
+	owner specified in the RMRK messages 
+	in the Kusama network.
+	This contract does not implement the full 
+	functionality of the RMRK TAE, but allows 
+	you to transfer ownership rights to TAE, 
+	which allows them to be used in the MOVR ecosystem
+	
+	Warning: ERC721 has been changed!
+*/
 contract Hub is ERC721, Ownable{
     using Counters for Counters.Counter;
     constructor() ERC721("Dublicate_RMRK","DRMRK"){}
@@ -61,49 +73,91 @@ contract Hub is ERC721, Ownable{
     //-----------------
     //Custom
     
+    struct Child{
+        string collectionId;
+        string equippedId;
+        string ID;
+        string metadata;
+        string metadata_rarity;
+    }
+    
     struct Order{
         string  UID;
         string Address;
     }
     
-    //Events
+    //Events:
+	//Emitted when order for 'UID' is created from 'Client' on RMRK.
     event createdRMRKOrder(address Client, string UID);
+	
+	//Emitted when order for 'UID' is created from 'Client' on MOVR.
     event createdMOVROrder(string Client, string UID);
+	
+	//Emitted when order for 'UID' is filled 'Client' on MOVR.
     event filledRMRKOrder(address Client, string UID);
+	
+	//Emitted when order for 'UID' is filled 'Client' on RMRK.
     event filledMOVROrder(string Client, string UID);
     
-    //Some variables
+    //Variables:
+	//Mapping from UID to ID.
     mapping (string => uint) private _matchingU;
+	
+	//Mapping from ID to UID.
     mapping (uint => string) private _matchingI;
+	
+	//Mapping from address Client to mapping from UID to bool.
+	//This storage opened orders on MOVR.
     mapping (address => mapping(string => bool)) private _ordersRMRK;
+	
+	//Mapping from Address on Kusama chain to bool.
+	//This storage opened orders on RMRK.
     mapping (string => bool)private _storageRMRK;
+	
+	//Mapping drom address Client to uint.
+	//This is a repository of overpaid money for a refund.
     mapping (address => uint)private _lostMoney;
-    mapping (string => string)private _storageMeta;
+	
+	//Mapping from UID to mapping from Number Child to Child.
+    mapping (string => mapping(uint => Child))private _storageMeta;
     
-    //pending
+	//Mapping from address to mapping from UID to URI.
+    //Storage completed orders, but not expected ones.
     mapping (address => mapping(string => string)) private _pendingOrderRMKR;
     
+	//Stores nft transfer orders requiring processing (from MOVR to RMRK).
     Order[] private _ordersMOVR;
     
+	//The amount of the commission for the payment of the server side.
     uint private _serverPayment = 0;
+	
+	//The amount of the commission for payment of the transaction.
     uint private _deployPayment = 0;
+	
+	//The address that creates the nft.
     address payable private _reciever = payable(0x43A2cCed6d1B7ba37173336AA794bAAB2cB1f830);
+	
+	//Address for receiving payments.
     address payable private _bank = payable(0xEc92783237998d58d3323880A5515A2a444Ed3E6);
     
     //Flags
+	//Enables and disables NFT transfer payment.
     bool private _paymentsOn = false; 
     
     //Functions
     
+	//Returns overpayments for the calling address.
     function myLostMoney()public view returns(uint){
         return _lostMoney[msg.sender];
     }
     
+	//Allows you to collect overpayments of the calling address.
     function takeMoney()public {
         require(_lostMoney[msg.sender]>0, "You didn't lose any money!");
         payable(msg.sender).transfer(_lostMoney[msg.sender]);
     }
     
+	//Commission payment function.
     function _pay()private returns (bool result){
         _reciever.transfer(_deployPayment);
         _bank.transfer(_serverPayment);
@@ -113,6 +167,13 @@ contract Hub is ERC721, Ownable{
         return true;
     }
     
+	/*
+	Creates an order for the transfer of NFT from RMRK to MOVR.
+	
+	Requirements:
+	Client - The recipient's address in the MOVR network;
+	UID - NFT ID RMRK;
+	*/
     function createRMRKOrder(address Client, string memory UID) public payable {
         if(_paymentsOn){
             require(msg.value >= _serverPayment + _deployPayment);
@@ -139,6 +200,13 @@ contract Hub is ERC721, Ownable{
         emit createdRMRKOrder(Client,UID);
     }
     
+	/*
+	Creates an order for the transfer of NFT from MOVR to RMRK.
+	
+	Requirements:
+	Client - The recipient's address in the Kusama chain;
+	UID - NFT ID RMRK;
+	*/
     function createMOVROrder(string memory Client, string memory UID)public payable{
         if(_paymentsOn){
             require(msg.value >= _serverPayment + _deployPayment);
@@ -158,9 +226,19 @@ contract Hub is ERC721, Ownable{
         emit createdMOVROrder(Client,UID);
     }
     
-    function fillRMRKOrder(address Client, string memory UID, string memory URI, string memory data) onlyOwner public{
+	/*
+	Closes the order (RMRK => MOVR)
+	If the NFT is expected on the MOVR side, then a duplicate of the NFT is created on the MOVR side and transferred to the waiting client
+	Otherwise, a duplicate is created on the MOVR side and is recorded in marked as a pending order
+	
+	Requirements:
+	Client - The recipient's address in the Kusama chain;
+	UID - NFT ID RMRK;
+	URI - Link to metadata;
+	data - Array of nested nfts
+	*/
+    function fillRMRKOrder(address Client, string memory UID, string memory URI, Child[] memory data) onlyOwner public{
         require(bytes(URI).length != 0);
-        require(bytes(data).length != 0);
         require(Client != address(0), "Dont fill 0 address!");
         
         if(_ordersRMRK[Client][UID]){
@@ -173,19 +251,30 @@ contract Hub is ERC721, Ownable{
             _setTokenURI(ID,URI);
             _storageRMRK[UID] = true;
             _ordersRMRK[Client][UID] = false;
-            _storageMeta[UID] = data;
+            for(uint i = 0; i < data.length; i = i + 1){
+                _storageMeta[UID][i]=data[i];
+            }
             
             emit filledRMRKOrder(Client,UID);
         }else{
             _storageRMRK[UID] = true;
             _pendingOrderRMKR[Client][UID]=URI;
-            _storageMeta[UID] = data;
+            for(uint i = 0; i < data.length; i = i + 1){
+                _storageMeta[UID][i]=data[i];
+            }
             
             emit filledRMRKOrder(Client,UID);
         }
     }
     
-
+	/*
+	Closes the orders (MOVR => RMRK)
+	
+	! Call only after orders are executed by the server side !
+	
+	Requirements:
+	Count - Count number of executed orders;
+	*/
     function fillMOVROrders(uint Count) onlyOwner public payable{
         for(uint i=0;i<Count;i= i+1){
             _ordersMOVR.pop();
@@ -193,6 +282,7 @@ contract Hub is ERC721, Ownable{
     }
     
     //Get Functions
+	//The functions return contract variables to display the contract status
     function getUID(uint ID)view public returns(string memory){
         return _matchingI[ID];
     }
@@ -206,8 +296,8 @@ contract Hub is ERC721, Ownable{
     }
     
     
-    function getMeta(uint ID)public view returns (string memory){
-        return _storageMeta[_matchingI[ID]];
+    function getMeta(uint ID, uint i)public view returns (Child memory){
+        return _storageMeta[_matchingI[ID]][i];
     }
     
     function getID(string memory UID)public view returns (uint){
@@ -232,13 +322,7 @@ contract Hub is ERC721, Ownable{
     }
     
     //Set Functions
-    function addMOVROrders(string memory Client, string memory UID) onlyOwner public{
-        Order memory order;
-        order.UID = UID;
-        order.Address = Client;
-        _ordersMOVR.push(order);
-    }
-    
+	//Function for setting up commissions
     function changeServerPayments(uint New) onlyOwner public{
         _serverPayment = New;
     }
