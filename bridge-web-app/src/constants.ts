4176baae674e6b62ebde63186faaef00cd07aa6b
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2021-11-26 10:34:58 +0000

    add the defaults

--- bridge-web-app/src/constants.ts
+++ bridge-web-app/src/constants.ts
@@ -1,14 +1,14 @@
 // polkadot RPC, that the Polkadot.js extension talks to
-export const WS_PROVIDER_URLS_PROD = process.env.WS_RPC_PROVIDER_URL;
+export const WS_PROVIDER_URLS_PROD = process.env.WS_RPC_PROVIDER_URL || "wss://kusama-rpc.polkadot.io";
 
 // bridge app, that sits in the kusama network and responsible for the NFT transferring
-export const MOVR_BRIDGE_ADDRESS_KUSAMA = process.env.KUSAMA_BRIDGE_ADDRESS;
+export const MOVR_BRIDGE_ADDRESS_KUSAMA = process.env.KUSAMA_BRIDGE_ADDRESS || "";
 
 // MOVR contract, that is responsible for game logic, for the deposit (preorder) and other stuff
-export const MOONBASE_CONTRACT = process.env.MOONBASE_CONTRACT;
+export const MOONBASE_CONTRACT = process.env.MOONBASE_CONTRACT || "";
 
 // Chain ID. In order to avoid sending tokens to real network, we force Metamask to switch to
 // exactly this network. Must be the numeric value. Specifically for the Moonbeam, please refer to the docs:
 // https://docs.moonbeam.network/builders/get-started/moonbase/#chain-id
-const moonbeamNetworkNumericID = process.env.MOONBEAM_NETWORK_ID;
+const moonbeamNetworkNumericID = process.env.MOONBEAM_NETWORK_ID || 0;
 export const TEST_MOONBEAM_NETWORK_ID = `0x${Number(moonbeamNetworkNumericID).toString(16)}`;
