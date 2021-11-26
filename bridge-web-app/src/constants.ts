2925366a187c935e4b6d9fad6e3512d56bf4ca7c
Ilja Hämäläinen <ilja.hamalainen@gmail.com>
2021-11-26 10:44:21 +0000

    float instead of int

--- bridge-web-app/src/constants.ts
+++ bridge-web-app/src/constants.ts
@@ -10,7 +10,7 @@ export const MOONBASE_CONTRACT = process.env.MOONBASE_CONTRACT || "";
 // Chain ID. In order to avoid sending tokens to real network, we force Metamask to switch to
 // exactly this network. Must be the numeric value. Specifically for the Moonbeam, please refer to the docs:
 // https://docs.moonbeam.network/builders/get-started/moonbase/#chain-id
-const moonbeamNetworkNumericID:number = parseInt(process.env.MOONBEAM_NETWORK_ID || "0", 10);
+const moonbeamNetworkNumericID:number = parseFloat(process.env.MOONBEAM_NETWORK_ID || "0");
 export const TEST_MOONBEAM_NETWORK_ID = `0x${Number(moonbeamNetworkNumericID).toString(16)}`;
 
 export const TRANSACTION_VALUE_IN_ETHER = process.env.VALUE_OF_CONTRACT_TRANSACTION_ETHER || "0";
